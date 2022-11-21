ffedc95f8a537dcfe65559a6c8d0b2338a2b81a3
Matthew McCune <matt@mattrick.org>
2022-11-20 23:46:30 -0800

    Change XDG Directories to lowercase

--- nix/home/matt/profiles/desktop/default.nix
+++ nix/home/matt/profiles/desktop/default.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ config, ... }: {
   imports = [
     ./modules/chromium.nix
     ./modules/discord.nix
@@ -10,6 +10,26 @@
   ];
 
   config = {
+    # Allow Bluetooth devices to control audio playback
     services.mpris-proxy.enable = true;
+
+    # I prefer lowercase directory names, so change XDG directories to use them
+    xdg.userDirs = {
+      enable = true;
+
+      desktop = "${config.home.homeDirectory}/desktop";
+      documents = "${config.home.homeDirectory}/documents";
+      download = "${config.home.homeDirectory}/downloads";
+      music = "${config.home.homeDirectory}/music";
+      pictures = "${config.home.homeDirectory}/pictures";
+      publicShare = "${config.home.homeDirectory}/public";
+      templates = "${config.home.homeDirectory}/templates";
+      videos = "${config.home.homeDirectory}/videos";
+
+      # Also have a projects directory for my projects
+      extraConfig = {
+        XDG_PROJECTS_DIR = "${config.home.homeDirectory}/projects";
+      };
+    };
   };
 }
