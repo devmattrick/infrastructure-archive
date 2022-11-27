a5168274827c0ca13889c0ad480c0a688e5e5281
Matthew McCune <matt@mattrick.org>
2022-11-27 00:19:54 -0800

    Temporarily disable lithium until hardware-configuration is available

--- nix/flake.nix
+++ nix/flake.nix
@@ -45,15 +45,15 @@
         specialArgs = inputs;
       };
 
-      "lithium" = nixpkgs.lib.nixosSystem {
-        inherit system;
+      # "lithium" = nixpkgs.lib.nixosSystem {
+      #   inherit system;
 
-        modules = [
-          ./system/hosts/lithium
-        ];
+      #   modules = [
+      #     ./system/hosts/lithium
+      #   ];
 
-        specialArgs = inputs;
-      };
+      #   specialArgs = inputs;
+      # };
     };
 
     homeConfigurations = {
@@ -65,32 +65,32 @@
         ];
       };
 
-      "matt@lithium" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+      # "matt@lithium" = home-manager.lib.homeManagerConfiguration {
+      #   inherit pkgs;
 
-        modules = [
-          ./home/matt/hosts/lithium.nix
-        ];
-      };
+      #   modules = [
+      #     ./home/matt/hosts/lithium.nix
+      #   ];
+      # };
     };
 
     deploy.nodes = {
-      sshUser = "deploy";
+      # sshUser = "deploy";
 
-      lithium = {
-        hostname = "lithium.lab.mattrick.org";
+      # lithium = {
+      #   hostname = "lithium.lab.mattrick.org";
 
-        profiles = {
-          system = {
-            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.lithium;
-          };
+      #   profiles = {
+      #     system = {
+      #       path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.lithium;
+      #     };
 
-          home-matt = {
-            user = "matt";
-            path = deploy-rs.lib.x86_64-linux.activate.home-manager self.homeConfigurations."matt@lithium";
-          };
-        };
-      };
+      #     home-matt = {
+      #       user = "matt";
+      #       path = deploy-rs.lib.x86_64-linux.activate.home-manager self.homeConfigurations."matt@lithium";
+      #     };
+      #   };
+      # };
     };
 
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
