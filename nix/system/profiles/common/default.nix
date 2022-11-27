ae43d1534e8bb45d372df026a71edd325c35366a
Matthew McCune <matt@mattrick.org>
2022-11-26 22:36:43 -0800

    Consolidate users definition

--- nix/system/profiles/common/default.nix
+++ nix/system/profiles/common/default.nix
@@ -32,19 +32,5 @@
 
     # Enable zsh shell completion for system packages
     environment.pathsToLink = ["/share/zsh"];
-
-    # Create users
-    users.users.matt = {
-      isNormalUser = true;
-
-      extraGroups = [
-        "wheel"
-        "networkmanager"
-      ];
-
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBJ7plqu/62IIaNdHf4QWn8zFM5sJAP0peftiheVOhI5"
-      ];
-    };
   };
 }
--- nix/system/profiles/common/modules/users.nix
+++ nix/system/profiles/common/modules/users.nix
@@ -6,6 +6,10 @@
 
         description = "Matt";
 
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBJ7plqu/62IIaNdHf4QWn8zFM5sJAP0peftiheVOhI5"
+        ];
+
         extraGroups = [
           "wheel"
           "networkmanager"
